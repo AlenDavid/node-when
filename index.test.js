da762ecd093d3d5d5095a6721865513e28df807b
David Alen <davidalen.dev@gmail.com>
2022-03-14 08:41:55 -0300

    add: exporting function from index file

--- index.test.js
+++ index.test.js
@@ -1,8 +1,11 @@
-const t = require("tap");
+const tap = require("tap");
 const when = require("./index");
 
-t.todo("function is exported by index.js file");
-t.todo("validates the truthiness of the first argument");
-t.todo("accepts a callback function as the second argument");
-t.todo("trigger the callback function when the first argument is truthy");
-t.todo("don't trigger the callback function when the first argument is falsy");
+tap.type(when, "function", "function is exported by index.js file");
+
+tap.todo("validates the truthiness of the first argument");
+tap.todo("accepts a callback function as the second argument");
+tap.todo("trigger the callback function when the first argument is truthy");
+tap.todo(
+  "don't trigger the callback function when the first argument is falsy"
+);
